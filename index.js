c165a26ef3b5ea7102dbb0cb48015f4f331c642a
David Sarma <davidsarma@gmail.com>
2021-12-17 04:49:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import { getDate } from "./src/util.js"
 
-addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request))
-})
+// addEventListener("fetch", event => {
+//   event.respondWith(handleRequest(event.request))
+// })
 
-async function handleRequest(request) {
-  return new Response(`Hello world, today is ${getDate()}`)
+// async function handleRequest(request) {
+//   return new Response(`Hello world, today is ${getDate()}`)
+// }
+
+export default {
+  async fetch(request, environment, context) {
+    return new Response("I’m a module!");
+  },
+  async scheduled(controller, environment, context) {
+    // await doATask();
+  }
 }
